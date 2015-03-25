41c0fbcaf483b0e20b40b153faf25e35103a7ae9
Nuno Gomes <nuno_andre_7@hotmail.com>
2015-03-25 21:16:01 +0000

    Initialize project
    Users controller already added;

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(first_name: 'Nuno', last_name: 'Gomes', username: 'nunogomes', password: 'password', email: 'nuno@hotmail.com')
+User.create!(first_name: 'Eduardo', last_name: 'Pinto', username: 'eduardopinto', password: 'password', email: 'eduardo@hotmail.com')
+User.create!(first_name: 'Nuno', last_name: 'Santos', username: 'nunosantos', password: 'password', email: 'santos@hotmail.com')
+User.create!(first_name: 'Frederico', last_name: 'Carvalho', username: 'fredcarvalho', password: 'password', email: 'frederico@hotmail.com')
