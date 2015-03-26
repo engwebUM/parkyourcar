f4e5ba58622deb673d78055043e66a6b32284862
Nuno Gomes <nuno_andre_7@hotmail.com>
2015-03-26 13:12:34 +0000

    Rubocop correction
    Corrected 13 errors.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,18 +5,18 @@ class UsersControllerTest < ActionController::TestCase
     @user = users(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
     assert_not_nil assigns(:users)
   end
 
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
   end
 
-  test "should create user" do
+  test 'should create user' do
     assert_difference('User.count') do
       post :create, user: { email: @user.email, first_name: @user.first_name, last_name: @user.last_name, password: @user.password, username: @user.username }
     end
@@ -24,22 +24,22 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to user_path(assigns(:user))
   end
 
-  test "should show user" do
+  test 'should show user' do
     get :show, id: @user
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get :edit, id: @user
     assert_response :success
   end
 
-  test "should update user" do
+  test 'should update user' do
     patch :update, id: @user, user: { email: @user.email, first_name: @user.first_name, last_name: @user.last_name, password: @user.password, username: @user.username }
     assert_redirected_to user_path(assigns(:user))
   end
 
-  test "should destroy user" do
+  test 'should destroy user' do
     assert_difference('User.count', -1) do
       delete :destroy, id: @user
     end
