7dc94be8575533b5a6860750377559132847bb0a
Nuno Gomes <nuno_andre_7@hotmail.com>
2015-03-27 17:01:19 +0000

    Use the new Ruby 1.9 hash syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,10 @@
 class User < ActiveRecord::Base
-	has_secure_password
+  has_secure_password
 
-	EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
+  EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
 
-	validates :first_name, :presence => true,
-						   :length => { :maximum => 25 }
-	validates :last_name, :presence => true,
-						  :length => { :maximum => 50 }
-	validates :username, :presence => { :within => 8..25 },
-						 :uniqueness => true
-	validates :email, :presence => true,
-					  :length => { :maximum => 100 },
-					  :format => EMAIL_REGEX,
+  validates :first_name, presence: true, length: { maximum: 25 }
+  validates :last_name, presence: true, length: { maximum: 50 }
+  validates :username, presence: { within: 8..25 }, uniqueness: true
+  validates :email, presence: true, length: { maximum: 100 }, format: EMAIL_REGEX
 end
