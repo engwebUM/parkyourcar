6077c99cc4f7341103c3b4dbc011dde11d4442b7
Nuno Gomes <nuno_andre_7@hotmail.com>
2015-03-29 02:37:44 +0100

    Static Pages added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do 
-  resources :posts
-
   devise_for :users
-
- root 'posts#index'
+  get 'static_pages/help'
+  get 'static_pages/faq'
+  root 'static_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
