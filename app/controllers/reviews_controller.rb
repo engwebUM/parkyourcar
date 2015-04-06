742eeee14f81a5889613d79dd2f5b3a7d8a49a52
eduardopinto <dudacya@gmail.com>
2015-04-07 00:19:48 +0100

    Space page - init

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,18 @@
+class ReviewsController < ApplicationController
+
+  # GET /reviews
+  def index
+  end
+
+  # GET /reviews/1
+  def show
+  end
+
+  # GET /reviews/new
+  def new
+  end
+
+  # GET /reviews/1/edit
+  def edit
+  end
+end
