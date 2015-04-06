1b61618bf02e020936ff0ae75be9c74921e8d0b0
eduardopinto <dudacya@gmail.com>
2015-04-06 16:03:37 +0100

    Create table reviews

---
+++ db/migrate/20150406144622_create_reviews.rb
@@ -0,0 +1,12 @@
+class CreateReviews < ActiveRecord::Migration
+  def change
+    create_table :reviews do |t|
+      t.belongs_to :user, index: true
+      t.belongs_to :space, index: true
+      t.integer :evaluation
+      t.string :text_comment
+      t.datetime :date
+      t.timestamps null: false
+    end
+  end
+end
