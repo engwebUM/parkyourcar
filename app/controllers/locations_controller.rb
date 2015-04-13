071a0c7a2e1098a10f19f9675bc5f5322ee95054
nunomsantos <nuno21nt@gmail.com>
2015-04-13 06:52:33 +0100

    Adds filtering and ordering options to locations view

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,13 +1,17 @@
 class LocationsController < ApplicationController
   before_action :set_location, only: [:show]
+  helper_method :filters_present?
 
   # GET /locations
   def index
     if location_present?
-      @spaces = Space.near(params[:location], params[:distance]).paginate(page: params[:page], per_page: 5)
+      @spaces = Space.near(params[:location], params[:distance])
+      filter_spaces
+      sort_spaces
     else
-      @spaces = Space.all.paginate(page: params[:page], per_page: 5)
+      @spaces = Space.all
     end
+    paginate_spaces
     load_space_markers
   end
 
@@ -16,6 +20,22 @@ class LocationsController < ApplicationController
   end
 
   private
+
+  def filters_present?
+    params[:date_from].present? || params[:date_until].present? || params[:sort].present?
+  end
+
+  def filter_spaces
+    @spaces = @spaces.filter_by(params[:date_from], params[:date_until])
+  end
+
+  def sort_spaces
+    @spaces = @spaces.sort_by(params[:sort])
+  end
+
+  def paginate_spaces
+    @spaces = @spaces.paginate(page: params[:page], per_page: 5)
+  end
 
   def location_present?
     params[:location].present?
--- app/models/space.rb
+++ app/models/space.rb
@@ -5,4 +5,28 @@ class Space < ActiveRecord::Base
   belongs_to :user
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
+
+  def self.filter_by(date_from, date_until)
+    filtered = Space.where(nil)
+    filtered = filtered.where('date_from <= ?', DateTime.parse(date_from)) if date_from.present?
+    filtered = filtered.where('date_until >= ?', DateTime.parse(date_until)) if date_until.present?
+    filtered
+  end
+
+  def self.sort_by(sort_param)
+    sorted = Space.where(nil)
+    if sort_param.to_i == 2
+      sorted = sorted.reorder(:price_hour)
+    elsif sort_param.to_i == 3
+      sorted = sorted.select('COUNT(reviews.id) AS reviews_count').
+               joins('LEFT JOIN reviews ON spaces.id = reviews.id').
+               group(:id).reorder('reviews_count')
+    end
+    sorted
+  end
+
+  def self.datetime_conv(datetime)
+    datetime_format = '%d/%m/%Y %I:%M %p'
+    DateTime.strptime(datetime, datetime_format)
+  end
 end
