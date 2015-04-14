b2f639457022f59bf9d0015e2f493abd42eb89f6
eduardopinto <dudacya@gmail.com>
2015-04-14 01:19:48 +0100

    Show space - Some features. Missing add/edit review, add booking and owner rating.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -14,5 +14,28 @@ class ReviewsController < ApplicationController
 
   # GET /reviews/1/edit
   def edit
+    @review = Review.find(params[:id])
   end
+
+  def create
+    @review = Review.new(review_params)
+
+    if @review.save
+      redirect_to @review
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @review = Review.find(params[:id])
+
+    if @review.update(review_params)
+      redirect_to @review
+    else
+      render 'edit'
+    end
+  end
+
+
 end
