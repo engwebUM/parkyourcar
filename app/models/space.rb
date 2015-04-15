42e6fca66449fad20a7b7abf394415a9a3fa6988
nunomsantos <nuno21nt@gmail.com>
2015-04-15 12:52:36 +0100

    Changes sort selector from numbers to descriptors

--- app/models/space.rb
+++ app/models/space.rb
@@ -16,19 +16,14 @@ class Space < ActiveRecord::Base
 
   def self.sort_by(sort_param)
     sorted = Space.where(nil)
-    if sort_param.to_i == 2
+    if sort_param == 'pri'
       sorted = sorted.reorder(:price_hour)
-    elsif sort_param.to_i == 3
+    elsif sort_param == 'rev'
       sorted = sorted.select('COUNT(reviews.id) AS reviews_count').
                joins('LEFT JOIN reviews ON spaces.id = reviews.id').
                group(:id).reorder('reviews_count')
     end
     sorted
-  end
-
-  def self.datetime_conv(datetime)
-    datetime_format = '%d/%m/%Y %I:%M %p'
-    DateTime.strptime(datetime, datetime_format)
   end
 
   def self.valid_date?(date)
