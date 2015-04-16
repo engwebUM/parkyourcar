6a284149ed4a0085e6820e4a3ee32d4d339a4b48
Nuno Gomes <nuno_andre_7@hotmail.com>
2015-04-16 23:19:11 +0100

    Pending bookings in dashboard added

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,5 +10,17 @@ class StaticPagesController < ApplicationController
   end
 
   def dashboard
+  	@pending_bookings_made = count_pending_bookings
+  	@user_spaces = count_user_spaces
+  end
+
+  private
+
+  def count_pending_bookings
+    current_user.bookings.where('state = ?', 'pending').count
+  end
+
+  def count_user_spaces
+    current_user.spaces.count
   end
 end
