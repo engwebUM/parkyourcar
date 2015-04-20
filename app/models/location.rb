ed6d2d5db70895c7a660d85f6f494099012c4491
eduardopinto <dudacya@gmail.com>
2015-04-20 15:07:19 +0100

    Fixing similar code on spaces and locations controllers

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,9 @@
-class Location < ActiveRecord::Base
+class Location
+  def self.load_space_markers(spaces)
+    hash = Gmaps4rails.build_markers(spaces) do |space, marker|
+      marker.lat space.latitude
+      marker.lng space.longitude
+      marker.infowindow space.address
+    end
+  end
 end
