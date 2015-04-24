7b7e64df858f24f20197f9dc31486ed1dd8b8218
Nuno Gomes <nuno_andre_7@hotmail.com>
2015-04-24 03:25:13 +0100

    Rubocop fix

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,19 +5,27 @@ class Booking < ActiveRecord::Base
   validates :date_from, presence: true
   validates :date_until, presence: true
 
-  validate :valid_datetimes
-  validate :valid_interval
+  validate :valid_dates_format
+  validate :valid_dates_interval
+  validate :valid_date_from_space_interval
+  validate :valid_date_until_space_interval
 
   private
 
-  def valid_datetimes
+  def valid_dates_format
     errors.add(:date_from, 'must be a valid datetime') unless date_from.to_datetime && date_from.to_datetime > DateTime.now.to_datetime
     errors.add(:date_until, 'must be a valid datetime') unless date_until.to_datetime
   end
 
-  def valid_interval
+  def valid_dates_interval
     errors.add(:date_from, 'cannot be higher than date until') unless date_from.to_datetime < date_until.to_datetime
+  end
+
+  def valid_date_from_space_interval
     errors.add(:date_from, 'must be between date from and date until') unless date_from.to_datetime > space.date_from.to_datetime
+  end
+
+  def valid_date_until_space_interval
     errors.add(:date_until, 'must be between date from and date until') unless date_until.to_datetime < space.date_until.to_datetime
   end
 end
