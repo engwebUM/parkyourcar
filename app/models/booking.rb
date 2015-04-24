b190ac0a651a5842ddd5267f4e84198cb78808d7
Nuno Gomes <nuno_andre_7@hotmail.com>
2015-04-24 03:17:57 +0100

    Added booking validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -11,11 +11,13 @@ class Booking < ActiveRecord::Base
   private
 
   def valid_datetimes
-    errors.add(:date_from, 'must be a valid datetime') unless date_from.to_datetime
+    errors.add(:date_from, 'must be a valid datetime') unless date_from.to_datetime && date_from.to_datetime > DateTime.now.to_datetime
     errors.add(:date_until, 'must be a valid datetime') unless date_until.to_datetime
   end
 
   def valid_interval
-    errors.add(:date_from, 'dates must be in valid interval') unless date_from.to_datetime <= date_until.to_datetime
+    errors.add(:date_from, 'cannot be higher than date until') unless date_from.to_datetime < date_until.to_datetime
+    errors.add(:date_from, 'must be between date from and date until') unless date_from.to_datetime > space.date_from.to_datetime
+    errors.add(:date_until, 'must be between date from and date until') unless date_until.to_datetime < space.date_until.to_datetime
   end
 end
