bff59e733dec51a928ae2581b895839b2b1f1b3b
Nuno Gomes <nuno_andre_7@hotmail.com>
2015-04-29 16:00:54 +0100

    Revert 'sign in' link to button
    Revert 'sign in' link to button;
    Fix space time display in edit form.

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -24,5 +24,5 @@ def sign_in_with(email, password)
   visit '/users/sign_in'
   fill_in 'user_email', with: email
   fill_in 'user_password', with: password
-  click_button 'Sign in'
+  click_button 'Sign In'
 end
