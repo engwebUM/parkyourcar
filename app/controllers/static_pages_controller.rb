76b640f49edf43fc53efcf7fae3c2051094f7ad8
nunomsantos <nuno21nt@gmail.com>
2015-04-29 01:31:58 +0100

    Adds links on dashboard to Proposals received and pending tables, as well as counters indicating the number of bookings in each of those states.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,14 +10,28 @@ class StaticPagesController < ApplicationController
   end
 
   def dashboard
-    @pending_bookings_made = count_pending_bookings
+    @pending_bookings_made = count_pending_bookings_made
+    @received_bookings_awaiting_approval = count_received_bookings_awaiting_approval
+    @received_bookings_pending = count_received_bookings_pending
     @user_spaces = count_user_spaces
   end
 
   private
 
-  def count_pending_bookings
-    current_user.bookings.where('state = ?', 'pending').count
+  def count_pending_bookings_made
+    current_user.bookings.where('state = ?', BookingsController::PENDING_STATE).count
+  end
+
+  def count_received_bookings_awaiting_approval
+    count_received_bookings(BookingsController::SENT_STATE)
+  end
+
+  def count_received_bookings_pending
+    count_received_bookings(BookingsController::PENDING_STATE)
+  end
+
+  def count_received_bookings(state)
+    current_user.spaces.joins(:bookings).where('state = ?', state).count
   end
 
   def count_user_spaces
